6c0ffaf43c838bf223ceffe831e08bea42790b9d
Matt Filetto <matt.filetto@gmail.com>
2014-04-04 00:39:05 -0700

    mach-msm: fix compile errors on board-holiday-keypad.c

--- arch/arm/mach-msm/htc/holiday/board-holiday-keypad.c
+++ arch/arm/mach-msm/htc/holiday/board-holiday-keypad.c
@@ -23,28 +23,29 @@
 #include <mach/gpio.h>
 
 #include "board-holiday.h"
-/*#include "proc_comm.h"*/
 
 #include <linux/mfd/pmic8058.h>
-#include <linux/input/pmic8058-keypad.h>
+#include <linux/moduleparam.h>
 
+/* Macros assume PMIC GPIOs start at 0 */
+#define PM8058_GPIO_BASE			NR_MSM_GPIOS
+#define PM8058_GPIO_PM_TO_SYS(pm_gpio)		(pm_gpio + PM8058_GPIO_BASE)
+#define PM8058_GPIO_SYS_TO_PM(sys_gpio)		(sys_gpio - PM8058_GPIO_BASE)
+#define PM8058_MPP_BASE			(PM8058_GPIO_BASE + PM8058_GPIOS)
+#define PM8058_MPP_PM_TO_SYS(pm_gpio)		(pm_gpio + PM8058_MPP_BASE)
+#define PM8058_MPP_SYS_TO_PM(sys_gpio)		(sys_gpio - PM8058_MPP_BASE)
+#define PM8058_IRQ_BASE				(NR_MSM_IRQS + NR_GPIO_IRQS)
+
+#define PM8901_GPIO_BASE			(PM8058_GPIO_BASE + \
+						PM8058_GPIOS + PM8058_MPPS)
+#define PM8901_GPIO_PM_TO_SYS(pm_gpio)		(pm_gpio + PM8901_GPIO_BASE)
+#define PM8901_GPIO_SYS_TO_PM(sys_gpio)		(sys_gpio - PM901_GPIO_BASE)
+#define PM8901_IRQ_BASE				(PM8058_IRQ_BASE + \
+						NR_PMIC8058_IRQS)
 static char *keycaps = "--qwerty";
 #undef MODULE_PARAM_PREFIX
 #define MODULE_PARAM_PREFIX "board_holiday."
 module_param_named(keycaps, keycaps, charp, 0);
-
-static void config_gpio_table(uint32_t *table, int len)
-{
-	int n, rc;
-	for (n = 0; n < len; n++) {
-		rc = gpio_tlmm_config(table[n], GPIO_CFG_ENABLE);
-		if (rc) {
-			pr_err("[keypad]%s: gpio_tlmm_config(%#x)=%d\n",
-				__func__, table[n], rc);
-			break;
-		}
-	}
-}
 
 static struct gpio_event_direct_entry holiday_keypad_input_map[] = {
 	{
@@ -59,21 +60,15 @@ static struct gpio_event_direct_entry holiday_keypad_input_map[] = {
 		.gpio = HOLIDAY_GPIO_KEY_VOL_DOWN,
 		.code = KEY_VOLUMEDOWN,
 	},
+	{
+		.gpio = HOLIDAY_GPIO_KEY_CAPTURE,
+		.code = KEY_CAMERA,
+	},
+	{
+		.gpio = PM8058_GPIO_PM_TO_SYS(HOLIDAY_GPIO_KEY_CAMAF),
+		.code = KEY_HP,
+	},
 };
-
-static void holiday_setup_input_gpio(void)
-{
-	uint32_t inputs_gpio_table[] = {
-		GPIO_CFG(HOLIDAY_GPIO_KEY_POWER, 0, GPIO_CFG_INPUT,
-			GPIO_CFG_PULL_UP, GPIO_CFG_4MA),
-		GPIO_CFG(HOLIDAY_GPIO_KEY_VOL_UP, 0, GPIO_CFG_INPUT,
-			GPIO_CFG_PULL_UP, GPIO_CFG_4MA),
-		GPIO_CFG(HOLIDAY_GPIO_KEY_VOL_DOWN, 0, GPIO_CFG_INPUT,
-			GPIO_CFG_PULL_UP, GPIO_CFG_4MA),
-	};
-
-	config_gpio_table(inputs_gpio_table, ARRAY_SIZE(inputs_gpio_table));
-}
 
 static struct gpio_event_input_info holiday_keypad_input_info = {
 	.info.func = gpio_event_input_func,
@@ -86,18 +81,36 @@ static struct gpio_event_input_info holiday_keypad_input_info = {
 #endif
 	.keymap = holiday_keypad_input_map,
 	.keymap_size = ARRAY_SIZE(holiday_keypad_input_map),
-	.setup_input_gpio = holiday_setup_input_gpio,
 };
+
+static void holiday_setup_input_gpio(void)
+{
+	int n;
+	int rc;
+
+	uint32_t inputs_gpio_table[] = {
+		GPIO_CFG(HOLIDAY_GPIO_KEY_POWER, 0, GPIO_CFG_INPUT, GPIO_CFG_PULL_UP, GPIO_CFG_4MA),
+		GPIO_CFG(HOLIDAY_GPIO_KEY_VOL_UP, 0, GPIO_CFG_INPUT, GPIO_CFG_PULL_UP, GPIO_CFG_4MA),
+		GPIO_CFG(HOLIDAY_GPIO_KEY_VOL_DOWN, 0, GPIO_CFG_INPUT, GPIO_CFG_PULL_UP, GPIO_CFG_4MA),
+		GPIO_CFG(HOLIDAY_GPIO_KEY_CAPTURE, 0, GPIO_CFG_INPUT, GPIO_CFG_PULL_UP, GPIO_CFG_4MA),
+	};
+
+	for (n = 0; n < ARRAY_SIZE(inputs_gpio_table); n++) {
+		rc = gpio_tlmm_config(inputs_gpio_table[n], GPIO_CFG_ENABLE);
+		if (rc) {
+			pr_err("[keypad]%s: gpio_tlmm_config(%#x)=%d\n",
+				__func__, inputs_gpio_table[n], rc);
+			break;
+		}
+	}
+}
 
 static struct gpio_event_info *holiday_keypad_info[] = {
 	&holiday_keypad_input_info.info,
 };
 
 static struct gpio_event_platform_data holiday_keypad_data = {
-	.names = {
-		"holiday-keypad",
-		NULL,
-	},
+	.name = "holiday-keypad",
 	.info = holiday_keypad_info,
 	.info_count = ARRAY_SIZE(holiday_keypad_info),
 };
@@ -109,14 +122,8 @@ static struct platform_device holiday_keypad_input_device = {
 		.platform_data	= &holiday_keypad_data,
 	},
 };
-/*
-static int holiday_reset_keys_up[] = {
-	KEY_VOLUMEUP,
-	0
-};
-*/
+
 static struct keyreset_platform_data holiday_reset_keys_pdata = {
-	/*.keys_up = holiday_reset_keys_up,*/
 	.keys_down = {
 		KEY_POWER,
 		KEY_VOLUMEDOWN,
@@ -136,6 +143,8 @@ int __init holiday_init_keypad(void)
 
 	if (platform_device_register(&holiday_reset_keys_device))
 		printk(KERN_WARNING "%s: register reset key fail\n", __func__);
+
+	holiday_setup_input_gpio();
 
 	return platform_device_register(&holiday_keypad_input_device);
 }
